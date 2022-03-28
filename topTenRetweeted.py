a611da284563406cbcda70111fe364559f8970e9
fmaturanaa <felipe.maturana@uc.cl>
2022-03-28 17:44:24 -0300

    feat(topTenRetweeted): function complete

---
+++ topTenRetweeted.py
@@ -0,0 +1,5 @@
+def topTenRetweeted (dataFrame):
+    top_ten_retweeted = dataFrame.sort_values('retweetCount', ascending=False)
+    top_ten_retweeted = top_ten_retweeted[:10]
+    top_ten_retweeted = top_ten_retweeted[['url', 'content', 'retweetCount']]
+    return top_ten_retweeted
